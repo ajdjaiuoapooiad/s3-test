cf6eaf77dd0d3be15cfc85d0205aafc12033f255
yukito <iyggf66974@yahoo.ne.jp>
2025-04-13 16:39:31 +0900

    ローカル　file保存　成功

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,12 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  config.active_storage.service_configurations = {
+    local: {
+      service: 'Disk',
+      root: Rails.root.join('storage')
+    }
+  }
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
